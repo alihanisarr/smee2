6c708889dae93eef01290c1a2c1d3f59cdc76aab
alihanisarr <aliha.nra619@gmail.com>
2026-06-22 13:50:20 +0500

    Adding Webhook “IDs”

--- server.py
+++ server.py
@@ -4,17 +4,23 @@ import uvicorn
 app = FastAPI()
 
 client: WebSocket = None
+subscribers = {}
 
-@app.post("/webhook")
-async def webhook(request: Request):
-    global client
-    data = await request.json()
-    if client is not None:
-        await client.send_json(data)    
-    return {"message":"hello"}
-
-@app.websocket("/tunnel")
-async def websocket_endpoint(websocket: WebSocket):
+@app.post("/webhook/{subscription_id}")
+async def webhook(subscription_id: str, request: Request):
+    if subscription_id is not None:
+        global client
+        data = await request.json()
+        subscribers[subscription_id] = data
+        if client is not None:
+            await client.send_json(data) 
+        return {"message":"hello"}   
+    else:   
+        print("Invalid endpoint, connection not accepted")
+        return
+    
+@app.websocket("/tunnel/{subscription_id}")
+async def websocket_endpoint(subscription_id: str, websocket: WebSocket):
     await websocket.accept()
     global client
     client = websocket
@@ -25,7 +31,6 @@ async def websocket_endpoint(websocket: WebSocket):
         client = None
 
     await websocket.send_text("Message received")
-
 
 if __name__ == "__main__":
     uvicorn.run("server:app", port=5000) 
