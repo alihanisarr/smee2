ab23f55463bbe6af29e13d40d6ad1a4d4cd9facc
alihanisarr <aliha.nra619@gmail.com>
2026-06-30 16:55:03 +0500

    added nginx.conf

--- app/server.py
+++ app/server.py
@@ -3,17 +3,19 @@ import uvicorn
 
 app = FastAPI()
 
-client: WebSocket = None
+clients = {}
 subscribers = {}
 
 @app.post("/webhook/{subscription_id}")
 async def webhook(subscription_id: str, request: Request):
     if subscription_id is not None:
-        global client
         data = await request.json()
         print("Webhook received: ", data)
 
         subscribers[subscription_id] = data
+        
+        client = clients[subscription_id]
+
         if client is not None:
             await client.send_json(data) 
             print("Data sent to websocket client")
@@ -27,15 +29,14 @@ async def webhook(subscription_id: str, request: Request):
 @app.websocket("/tunnel/{subscription_id}")
 async def websocket_endpoint(subscription_id: str, websocket: WebSocket):
     await websocket.accept()
-    global client
-    client = websocket
+    clients[subscription_id] = websocket
     try:
         while True:
             data = await websocket.receive_text()
+            await websocket.send_text("Message received")
     except Exception as e:
-        client = None
+        clients.pop(subscription_id, None)
 
-    await websocket.send_text("Message received")
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=5000) 
